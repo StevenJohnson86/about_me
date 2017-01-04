cc1c3f719299bf8a574925054acf2d3ce466858f
Steven Johnson <stevenjohnson86@gmail.com>
2017-01-03 17:04:20 -0800

    Add console log responses for outcomes

--- app.js
+++ app.js
@@ -16,33 +16,49 @@ if (userName === null) {
   console.log('User does not want to play game');
 } else {
   var answer1 = prompt('Thanks ' + userName + '! First question is easy:\nTell me Y or N, Have I traveled to more than 5 countries?');
-  console.log(userName + ' answered ' + answer1 + ' to question 1. The correct answer is Y');
+  console.log(userName + ' answered ' + answer1 + ' to question 1.');
   if (answer1 === 'Y' || answer1 === 'y') {
     alert('Great guess! \nI\'ve been to Canada, Japan, S.Korea, Germany, Switzerland, Italy, and France!');
+    console.log(answer1 + ' is correct');
   } else {
     alert('Oops! I guess I forgot to tell you that I\'ve been to Canada, Japan, S.Korea, Germany, Switzerland, Italy, and France!');
+    console.log(answer1 + ' is wrong');
   }
   var answer2 = prompt('Next question:\nY of N, I grew up in Seattle, WA?');
-  console.log(userName + ' answered ' + answer2 + ' to question 2. The correct answer is N');
+  console.log(userName + ' answered ' + answer2 + ' to question 2.');
   if (answer2 === 'Y' || answer2 === 'y') {
     alert('Close guess! I was brought up in the Snoqualmie Valley, which is about 1 hour from Seattle.');
+    console.log(answer2 + ' is wrong');
   } else {
     alert('Correct! I grew up in the Snoqualmie Valley, which is about 1 hour from Seattle.');
+    console.log(answer2 + ' is correct');
   }
   var answer3 = prompt('Let\'s try another one!\nY or N, ####');
-  console.log(userName + ' answered ' + answer3 + ' to question 3. The correct answer is ###');
+  console.log(userName + ' answered ' + answer3 + ' to question 3.');
   if (answer3 === 'Y' || answer3 === 'y') {
     alert('');
+    console.log(answer3 + 'is ####');
   } else {
     alert('');
+    console.log(answer3 + 'is ####');
   }
   var answer4 = prompt('Second to last question:\nY or N, ####');
-  console.log(userName + ' answered ' + answer4 + ' to question 4. The correct answer is ###');
+  console.log(userName + ' answered ' + answer4 + ' to question 4.');
   if (answer3 === 'Y' || answer3 === 'y') {
     alert('');
+    console.log(answer4 + 'is ####');
   } else {
     alert('');
+    console.log(answer4 + 'is ####');
   }
   var answer5 = prompt('Final question:\nY or N, ####');
-  console.log(userName + ' answered ' + answer5 + ' to question 5. The correct answer is ###')
+  console.log(userName + ' answered ' + answer5 + ' to question 5.');
+  if (answer5 === 'Y' || answer5 === 'y') {
+    alert('');
+    console.log(answer5 + 'is ####');
+  } else {
+    alert('');
+    console.log(answer5 + 'is ####');
+  }
+  alert('Thank you for playing my game! Hope you enjoy your stay!');
 }
